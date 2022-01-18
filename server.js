853b4b98e30303e92bddf8eb8e87b44be1985e21
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2022-01-18 14:12:17 -0500

    file setup

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require(express)
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT);
