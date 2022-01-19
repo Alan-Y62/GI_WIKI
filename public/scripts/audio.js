333bf8428f414332d635a8718c2421929d5ad49d
Andy N <32938967+Exarr00@users.noreply.github.com>
2022-01-18 22:44:33 -0500

    music added

---
+++ public/scripts/audio.js
@@ -0,0 +1,19 @@
+const audio = document.getElementById('the_audio')
+audio.volume = 0.1;
+let vol =  false;
+const sound = document.getElementById('volume_control')
+
+sound.addEventListener('click' , () => {
+    if(vol){
+        audio.volume = 0;
+        vol = !vol;
+        sound.innerText = 'OFF'
+        sound.classList.remove('on')
+    } else {
+        audio.play();
+        audio.volume = 0.1
+        vol = !vol;
+        sound.innerText = 'ON'
+        sound.classList.add('on')
+    }
+})
