e2a6e2afbfa1bd7859eddf5185d98ba997a25af2
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2022-02-12 20:24:12 -0500

    created individual weapon page

--- routes/index.js
+++ routes/index.js
@@ -11,58 +11,18 @@ router.get("/", (req, res) => {
 
 router.get("/weapons", async (req, res) => {
   const weaponList = await Weapon.find().sort({ type: 1, rarity: 1, name: 1 });
-  res.render("weapons", { weapons: weaponList });
+  res.render("./weapons/weapons", { weapons: weaponList });
+});
+
+router.get("/weapons/:weapon_name", async (req, res) => {
+  let wname = req.params.weapon_name.replace("_", " ");
+  let weapon = await Weapon.findOne({ name: wname });
+  console.log(weapon);
+  res.render("./weapons/weapon", { weapon: weapon });
 });
 
 router.get("/privacy", (req, res) => {
   res.render("privacy");
-});
-
-router.get("/temp", (req, res) => {
-  res.render("temp");
-});
-
-router.post("/temp", (req, res) => {
-  const {
-    name,
-    rarity,
-    base_atk,
-    max_atk,
-    sub_stat,
-    base_sub,
-    max_sub,
-    asc,
-    ral1,
-    ral2,
-    ral3,
-    ral4,
-    ral5,
-    lore,
-  } = req.body;
-
-  const asc_mats = [];
-  asc_mats.push(...asc.split(","));
-
-  const ref_lvls = [];
-  if (ral1.length > 0) {
-    ref_lvls.push(ral1, ral2, ral3, ral4, ral5);
-  }
-
-  const weapon = new Weapon({
-    name: name,
-    rarity: rarity,
-    base_atk: base_atk,
-    maxed_atk: max_atk,
-    sub_stat: sub_stat,
-    sub_base_value: base_sub,
-    sub_max_value: max_sub,
-    ascension_mats: asc_mats,
-    refinement_abilities: ref_lvls,
-    lore: lore,
-  });
-  weapon.save();
-  console.log(weapon);
-  res.redirect("/temp");
 });
 
 module.exports = router;
