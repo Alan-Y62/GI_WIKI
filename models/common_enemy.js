503b0040f0c3bc305e2578ec7c855dee7e2258b3
Andy N <32938967+Exarr00@users.noreply.github.com>
2022-03-06 22:20:29 -0500

    ignore just a check to see if the datas are fixed

--- models/common_enemy.js
+++ models/common_enemy.js
@@ -10,7 +10,7 @@ const commonmobSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    element: [{
+    elements: [{
         type: String,
         required:true
     }],
